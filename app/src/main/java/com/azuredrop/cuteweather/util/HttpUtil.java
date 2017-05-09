78be2f44e9ac09a843d9eecb8d0d6828e9631ba3
AzureDrop <taozhanbiao@gmail.com>
2017-05-09 17:38:43 +0800

    完成遍历省市县三级列表功能

---
+++ app/src/main/java/com/azuredrop/cuteweather/util/HttpUtil.java
@@ -0,0 +1,25 @@
+package com.azuredrop.cuteweather.util;
+
+/**
+ * Created by AzureDrop on 2017/5/9.
+ */
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Http类
+ * @author AzureDrop
+ */
+public class HttpUtil {
+    /**
+     * 发送Http请求
+     * @param address url地址
+     * @param callback 用来处理响应的回调
+     */
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        okHttpClient.newCall(request).enqueue(callback);
+    }
+}
