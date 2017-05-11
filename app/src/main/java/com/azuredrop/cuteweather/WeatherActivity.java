b60d6825b87d6fa9cdf64d7fbb976068005124e9
AzureDrop <taozhanbiao@gmail.com>
2017-05-11 12:02:49 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/azuredrop/cuteweather/WeatherActivity.java
+++ app/src/main/java/com/azuredrop/cuteweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.azuredrop.cuteweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 
 import com.azuredrop.cuteweather.gson.Forecast;
 import com.azuredrop.cuteweather.gson.Weather;
+import com.azuredrop.cuteweather.service.AutoUpdateService;
 import com.azuredrop.cuteweather.util.HttpUtil;
 import com.azuredrop.cuteweather.util.Utility;
 import com.bumptech.glide.Glide;
@@ -48,7 +50,7 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * 申请的APIKey
      */
-    private static final String HEFENG_WEATHER_APIKEY = "f28feb687e594050bc145b72e92e298a";
+    public static final String HEFENG_WEATHER_APIKEY = "f28feb687e594050bc145b72e92e298a";
 
     /**
      * 当前城市的WeatherId
@@ -77,7 +79,7 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // 使背景扩展到系统状态栏
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
@@ -87,9 +89,9 @@ public class WeatherActivity extends AppCompatActivity {
 
         // 初始化控件
         backgoundImg = (ImageView) findViewById(R.id.bg_img);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         // SwipeRefreshLayout
-        swipeRefresh = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -98,7 +100,7 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
-        navButton = (Button)findViewById(R.id.nav_button);
+        navButton = (Button) findViewById(R.id.nav_button);
         navButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -239,6 +241,10 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
 
         weatherLayout.setVisibility(View.VISIBLE);
+
+        // 启动自动更新服务
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
