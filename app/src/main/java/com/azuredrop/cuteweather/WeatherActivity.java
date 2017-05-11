aa1f63793088c25ffae62fc632f7d17a61e9b0cd
AzureDrop <taozhanbiao@gmail.com>
2017-05-11 09:44:41 +0800

    添加获取Bing背景

--- app/src/main/java/com/azuredrop/cuteweather/WeatherActivity.java
+++ app/src/main/java/com/azuredrop/cuteweather/WeatherActivity.java
@@ -1,11 +1,14 @@
 package com.azuredrop.cuteweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -15,6 +18,7 @@ import com.azuredrop.cuteweather.gson.Forecast;
 import com.azuredrop.cuteweather.gson.Weather;
 import com.azuredrop.cuteweather.util.HttpUtil;
 import com.azuredrop.cuteweather.util.Utility;
+import com.bumptech.glide.Glide;
 
 import java.io.IOException;
 import java.io.PipedReader;
@@ -32,6 +36,10 @@ public class WeatherActivity extends AppCompatActivity {
      * 存放进SharedPreferences里返回的城市数据
      */
     public static final String SHARED_PREF_WEATHER = "weather";
+    /**
+     * 存放进SharedPreferences里背景图片
+     */
+    public static final String SHARED_PREF_BG_IMG = "background_img";
 
     /**
      * 申请的APIKey
@@ -39,6 +47,7 @@ public class WeatherActivity extends AppCompatActivity {
     private static final String HEFENG_WEATHER_APIKEY = "f28feb687e594050bc145b72e92e298a";
 
     // 控件
+    private ImageView backgoundImg;
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -54,9 +63,17 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
 
         // 初始化控件
+        backgoundImg = (ImageView) findViewById(R.id.bg_img);
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
         titleCity = (TextView) findViewById(R.id.title_city);
         titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
@@ -71,6 +88,14 @@ public class WeatherActivity extends AppCompatActivity {
 
         // 填充数据
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // 填充背景图片
+        String backgroundPic = preferences.getString(SHARED_PREF_BG_IMG, null);
+        if (backgroundPic != null) {
+            Glide.with(this).load(backgroundPic).into(backgoundImg);
+        } else {
+            loadBackgoundPic();
+        }
+        // 填充天气数据
         String weatherString = preferences.getString(SHARED_PREF_WEATHER, null);
         if (weatherString != null) {
             // 有缓存时直接解析天气数据
@@ -123,6 +148,9 @@ public class WeatherActivity extends AppCompatActivity {
                 });
             }
         });
+
+        // 请求天气信息的时候，刷新背景图片
+        loadBackgoundPic();
     }
 
     /**
@@ -175,5 +203,33 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
 
         weatherLayout.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 加载背景图片
+     */
+    private void loadBackgoundPic(){
+        String requestPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bgPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager
+                        .getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString(SHARED_PREF_BG_IMG, bgPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bgPic).into(backgoundImg);
+                    }
+                });
+            }
+        });
     }
 }
